95741844c68fce4d5de1ef293a7e2ed2e19c7110
ibaluk <ibaluk@kwanza-systems.ru>
2012-11-02 19:12:35 +0300

    DBTOOL-2 Корректировка исключений, в работе с блобами. getBlobInputStream()возвращает null если запись в таблице не найдена.

---
+++ src/main/java/ru/kwanza/dbtool/blob/StreamException.java
@@ -0,0 +1,25 @@
+package ru.kwanza.dbtool.blob;
+
+/**
+ * @author: Ivan Baluk
+ */
+abstract class StreamException extends Exception {
+    protected StreamException(String message) {
+        super(message);
+    }
+
+    static class RecordNotFoundException extends StreamException {
+
+        RecordNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    static class EmptyFieldException extends StreamException {
+
+        EmptyFieldException(String message) {
+            super(message);
+        }
+    }
+
+}
