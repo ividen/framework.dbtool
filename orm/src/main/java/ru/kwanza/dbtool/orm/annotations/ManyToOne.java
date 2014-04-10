43bfdc5a6970118d04dc0608b8098c21ef8198c9
aguzanov <aguzanov@kwanza-systems.ru>
2014-04-10 12:38:24 +0300

    Описание javadocs для ManyToOne

--- orm/src/main/java/ru/kwanza/dbtool/orm/annotations/ManyToOne.java
+++ orm/src/main/java/ru/kwanza/dbtool/orm/annotations/ManyToOne.java
@@ -8,9 +8,36 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Описывает взаимосвязь между двумя сущностями
+ * Описывает взаимосвязь между двумя сущностями типа <b>многие к одному</b>
+ * <p/>
+ * Пример:
+ * <pre>{@code @Entity(name="EntityA", table="entity_A")
+ *  public class EntityA{
+ *    @literal @IdField("id")
+ *     private Long id;
+ *          .....
+ *  }
  *
+ * @author Guzanov Alexander
  * @author Alexander Guzanov
+ * @literal @Entity(name="EntityB", table="entity_B")
+ * public class EntityB{
+ *      .....
+ * @literal @Field("entity_a_id")
+ * private Long entityAId;
+ * @literal @ManyToOne(property="entityAId")
+ * private EntityA entityA
+ *      .....
+ * }
+ * }</pre>
+ * Стоит заметить, что поля связи являются немутабельными: т.е для установки связей, нужно либо создавать сущность, либо заполнять соотвествующее поля ссылки.
+ * Пример:
+ * <pre>{@code @Entity(name="EntityA", table="entity_A")
+ * EntityB entityB = ...;
+ * EntityA entityA = ...;
+ * entityB.setEntityAId(entityA.getCode());// work
+ * entityB.setEntityA(entityA); // do't work
+ * }</pre>
  */
 @Retention(RUNTIME)
 @Target({FIELD, METHOD})
