9a4170789556f8f81dea166147e858c991022628
aguzanov <aguzanov@kwanza-systems.ru>
2014-04-10 12:07:20 +0300

    Описание javadocs для Entity

--- orm/src/main/java/ru/kwanza/dbtool/orm/annotations/AbstractEntity.java
+++ orm/src/main/java/ru/kwanza/dbtool/orm/annotations/AbstractEntity.java
@@ -64,5 +64,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target(TYPE)
 public @interface AbstractEntity {
+    /**
+     * Имя сущности
+     */
     String name();
 }
