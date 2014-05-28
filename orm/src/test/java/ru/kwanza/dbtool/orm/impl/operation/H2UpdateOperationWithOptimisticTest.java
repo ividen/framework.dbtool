7c035267264629e49447c091b591e8b7a19e6ec6
aguzanov <aguzanov@kwanza-systems.ru>
2014-05-28 17:49:36 +0300

    H2 dbtool.orm операциии с сущностями

---
+++ orm/src/test/java/ru/kwanza/dbtool/orm/impl/operation/H2UpdateOperationWithOptimisticTest.java
@@ -0,0 +1,10 @@
+package ru.kwanza.dbtool.orm.impl.operation;
+
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author Kiryl Karatsetski
+ */
+@ContextConfiguration(locations = "mssql-config.xml")
+public class H2UpdateOperationWithOptimisticTest extends UpdateOperationWithOptimisticTest {
+}
